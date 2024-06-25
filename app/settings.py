60b1a77f2cca5809d98ecf76e0073c9983c1a047
romanvlad <romaliiskii.v@gmail.com>
2024-06-25 21:54:52 +0300

    Debug-toolbar added

--- app/settings.py
+++ app/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'debug_toolbar',
+
     'main',
     'goods',
+
 ]
 
 MIDDLEWARE = [
@@ -50,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -122,6 +127,12 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- app/urls.py
+++ app/urls.py
@@ -17,8 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from app.settings import DEBUG
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
     path('catalogue/', include('goods.urls', namespace='catalogue')),
 ]
+
+if DEBUG:
+    urlpatterns += [
+        # ...
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
