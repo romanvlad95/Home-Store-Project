7c17edac77f9c4b9d5b0425c9f7671c1e3c2fd82
romanvlad <romaliiskii.v@gmail.com>
2024-06-26 22:16:23 +0300

    Added final pagination option GET params

--- goods/urls.py
+++ goods/urls.py
@@ -22,6 +22,5 @@ app_name = 'goods'
 
 urlpatterns = [
     path('<slug:category_slug>/', views.catalogue, name='index'),
-    path('<slug:category_slug>/<int:page>/', views.catalogue, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 ]
