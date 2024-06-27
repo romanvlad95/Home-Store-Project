4f95bbd7741f9aaecef7d6c4f31442682ea0008a
romanvlad <romaliiskii.v@gmail.com>
2024-06-27 21:35:41 +0300

    Simple search created. Deleted sqlite

--- goods/urls.py
+++ goods/urls.py
@@ -21,6 +21,7 @@ from goods import views
 app_name = 'goods'
 
 urlpatterns = [
+    path('search/', views.catalogue, name='search'),
     path('<slug:category_slug>/', views.catalogue, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 ]
