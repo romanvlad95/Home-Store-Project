93b838bca2258831c5e1641fce43875b3abf3fa5
romanvlad <romaliiskii.v@gmail.com>
2024-06-29 05:14:44 +0300

    Profile form, controller & notifications are ready.

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from users.models import User
 
@@ -8,19 +8,42 @@ class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ('username', 'password')
-    # username = forms.CharField()
-    # password = forms.CharField()
 
-    # username = forms.CharField(
-    #     label='Username:',
-    #     widget=forms.TextInput(attrs={'autofocus': True,
-    #                                   'class': 'form-control',
-    #                                   'placeholder': 'Enter username'}),
-    # )
-    # password = forms.CharField(
-    #     label='Password:',
-    #     widget=forms.PasswordInput(attrs={'autocomplete': 'current-password',
-    #                                       'class': 'form-control',
-    #                                       'placeholder': 'Enter password'}),
-    # )
 
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2'
+        )
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
+
+class ProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = (
+            'image',
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+        )
+
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
