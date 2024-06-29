6674c94da4ab3dd37a23158fcb473157184e8eec
romanvlad <romaliiskii.v@gmail.com>
2024-06-29 19:14:22 +0300

    Add to cart view, refactor included cart templates

--- users/views.py
+++ users/views.py
@@ -18,8 +18,10 @@ def login(request):
                 auth.login(request, user)
                 messages.success(request, f'{username} - You are now logged in')
 
-                if request.POST.get('next', None):
+                redirect_page = request.POST.get('next', None)
+                if redirect_page and redirect_page != reverse('users:logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
+
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
