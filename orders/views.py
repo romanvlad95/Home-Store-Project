baaa6a5b91aba48e212f0f2a91541b0260d08219
romanvlad <romaliiskii.v@gmail.com>
2024-07-04 21:35:31 +0300

    Class view refactor - Cart refactoring. Final commit in this section. The project now is class based

--- orders/views.py
+++ orders/views.py
@@ -1,70 +1,78 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from django.db import transaction
 from django.forms import ValidationError
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+
 from carts.models import Cart
 
 from orders.forms import CreateOrderForm
 from orders.models import Order, OrderItem
 
 
-@login_required
-def create_order(request):
-    if request.method == 'POST':
-        form = CreateOrderForm(data=request.POST)
-        if form.is_valid():
-            try:
-                with transaction.atomic():
-                    user = request.user
-                    cart_items = Cart.objects.filter(user=user)
+class CreateOrderView(LoginRequiredMixin, FormView):
+    template_name = 'orders/create_order.html'
+    form_class = CreateOrderForm
+    success_url = reverse_lazy('users:profile')
 
-                    if cart_items.exists():
-                        order = Order.objects.create(
-                            user=user,
-                            phone_number=form.cleaned_data['phone_number'],
-                            requires_delivery=form.cleaned_data['requires_delivery'],
-                            delivery_address=form.cleaned_data['delivery_address'],
-                            payment_on_get=form.cleaned_data['payment_on_get'],
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['first_name'] = self.request.user.first_name
+        initial['last_name'] = self.request.user.last_name
+        return initial
+
+    def form_valid(self, form):
+        try:
+            with transaction.atomic():
+                user = self.request.user
+                cart_items = Cart.objects.filter(user=user)
+
+                if cart_items.exists():
+                    order = Order.objects.create(
+                        user=user,
+                        phone_number=form.cleaned_data['phone_number'],
+                        requires_delivery=form.cleaned_data['requires_delivery'],
+                        delivery_address=form.cleaned_data['delivery_address'],
+                        payment_on_get=form.cleaned_data['payment_on_get'],
+                    )
+                    for cart_item in cart_items:
+                        product = cart_item.product
+                        name = cart_item.product.name
+                        price = cart_item.product.sell_price()
+                        quantity = cart_item.quantity
+
+                        if product.quantity < quantity:
+                            raise ValidationError(f'Insufficient quantity of product {name} in stock.\
+                                                   Available - {product.quantity}')
+
+                        OrderItem.objects.create(
+                            order=order,
+                            product=product,
+                            name=name,
+                            price=price,
+                            quantity=quantity,
                         )
-                        for cart_item in cart_items:
-                            product = cart_item.product
-                            name = cart_item.product.name
-                            price = cart_item.product.sell_price()
-                            quantity = cart_item.quantity
+                        product.quantity -= quantity
+                        product.save()
 
-                            if product.quantity < quantity:
-                                raise ValidationError(f'Insufficient quantity of product {name} in stock.\
-                                                        Available - {product.quantity}')
 
-                            OrderItem.objects.create(
-                                order=order,
-                                product=product,
-                                name=name,
-                                price=price,
-                                quantity=quantity,
-                            )
-                            product.quantity -= quantity
-                            product.save()
+                    cart_items.delete()
 
-                        cart_items.delete()
+                    messages.success(self.request, 'Order is successfully created')
+                    return redirect('user:profile')
+        except ValidationError as e:
+            messages.success(self.request, str(e))
+            return redirect('orders:create_order')
 
-                        messages.success(request, 'Order placed successfully!')
-                        return redirect('user:profile')
-            except ValidationError as e:
-                messages.success(request, str(e))
-                return redirect('orders:create_order')
-    else:
-        initial = {
-            'first_name': request.user.first_name,
-            'last_name': request.user.last_name,
-        }
+    def form_invalid(self, form):
+        messages.error(self.request, 'All fields are required.')
+        return redirect('orders:create_order')
 
-        form = CreateOrderForm(initial=initial)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Finalizing order'
+        context['order'] = True
+        return context
 
-    context = {
-        'title': 'Home - Order Placement',
-        'form': form,
-        'order': True,
-    }
-    return render(request, 'orders/create_order.html', context=context)
